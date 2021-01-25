59b3f4af657ff2fcf7e8a1026e0c16f65d301fa3
Rahul Yadav <ryadav@ex2india.com>
2021-01-25 09:20:32 +0530

    modify: implement project structure

--- main.js
+++ main.js
@@ -1,60 +1,158 @@
 "use strict";
-var require;
 var read = require("prompt-sync")();
+var printString = function (str) {
+    console.log(str);
+};
+var debug = function (str) {
+    // console.log(str);
+};
+var takeInput = function (str) {
+    var input = read("" + str);
+    input.trim().toLowerCase();
+    return input;
+};
+var generateRandom = function (end, start) {
+    if (start === void 0) { start = 0; }
+    return Math.floor(Math.random() * end) + start;
+};
+var choice;
+(function (choice) {
+    choice[choice["rock"] = 0] = "rock";
+    choice[choice["paper"] = 1] = "paper";
+    choice[choice["scissors"] = 2] = "scissors";
+    choice[choice["lizard"] = 3] = "lizard";
+    choice[choice["spock"] = 4] = "spock";
+})(choice || (choice = {}));
 var Player = /** @class */ (function () {
     function Player(move) {
         this.move = move;
+        this.score = 0;
     }
     Player.prototype.getChoice = function () {
         return this.move;
     };
+    Player.prototype.getScore = function () {
+        return this.score;
+    };
+    Player.prototype.incrementScore = function () {
+        this.score += 1;
+    };
     return Player;
-}());
-var Input = /** @class */ (function () {
-    function Input() {
-    }
-    Input.prototype.getUserInput = function () {
-        return this.userInput;
-    };
-    Input.prototype.takeUserInput = function () {
-        // takes input from user;
-    };
-    return Input;
 }());
 var RockPaperRules = /** @class */ (function () {
     function RockPaperRules() {
+        this.instructionsArray = [
+            "Scissors cuts Paper",
+            "Paper covers Rock",
+            "Rock crushes Lizard",
+            "Lizard poisons Spock",
+            "Spock smashes Scissors",
+            "Scissors decapitates Lizard",
+            "Lizard eats Paper",
+            "Paper disproves Spock",
+            "Spock vaporizes Rock",
+            "Rock crushes Scissors",
+        ];
+        this.solutionSet = new Map();
+        this.generateSolutionSet();
+        this.printMap();
     }
-    RockPaperRules.prototype.winConditions = function (inputOne, inputTwo) {
-        return "abc";
+    RockPaperRules.prototype.printMap = function () {
+        debug("this is my map..");
+        this.solutionSet.forEach(function (key, value) {
+            debug("key: " + key + " and value: " + value);
+        });
+    };
+    RockPaperRules.prototype.generateSolutionSet = function () {
+        for (var i in this.instructionsArray) {
+            var partial = this.instructionsArray[i]
+                .toLowerCase()
+                .split(" ");
+            var key = partial[0] + "," + partial[2];
+            var value = partial[0] + "," + partial[1];
+            this.solutionSet.set(key, value);
+            // debug("Setting value1: " + key + " " + value);
+            key = partial[2] + "," + partial[0];
+            this.solutionSet.set(key, value);
+            // debug("Setting value2: " + key + " " + value);
+        }
+    };
+    RockPaperRules.prototype.getValueForKey = function (key) {
+        debug("key to be processed: " + key);
+        debug("check if exist: " + this.solutionSet.has(key));
+        if (this.solutionSet.has(key)) {
+            var value = this.solutionSet.get(key);
+            debug("returning value: " + value);
+            return value;
+        }
+        return "draw";
     };
     RockPaperRules.prototype.printInstructions = function () {
-        //print instuctions;
+        printString("\nThis is a 2 player game and you are playing against computer...All the best!");
+        for (var i in this.instructionsArray) {
+            var partialInstruction = this.instructionsArray[i];
+            printString(i + ". " + partialInstruction);
+        }
+        printString("\n** Enter your choice in lower case\n");
     };
     return RockPaperRules;
 }());
 var RockPaperJudge = /** @class */ (function () {
     function RockPaperJudge() {
+        this.gameRules = new RockPaperRules();
     }
-    RockPaperJudge.prototype.checkResult = function (inputOne, inputTwo) {
-        return "abc";
+    RockPaperJudge.prototype.checkResult = function (input1, input2) {
+        var answer = this.gameRules.getValueForKey(input1 + "," + input2);
+        var ans = answer.split(",");
+        debug("ans received: " + ans);
+        var result = "";
+        if (ans[0] === input1) {
+            result += "\n" + ans[0] + " " + ans[1] + " " + input2 + " \nCongratulations You Won!";
+        }
+        else if (ans[0] === input2) {
+            result += "\n" + ans[0] + " " + ans[1] + " " + input1 + " \nComputer Won...Better luck next time!";
+        }
+        else {
+            result += "\nNobody Wins...Draw!";
+        }
+        printString(result + "\n");
+    };
+    RockPaperJudge.prototype.isLegalChoice = function (userChoice) {
+        if (userChoice in choice) {
+            return true;
+        }
+        printString("Please Enter Valid Input..");
+        return false;
+    };
+    RockPaperJudge.prototype.printRules = function () {
+        this.gameRules.printInstructions();
     };
     return RockPaperJudge;
 }());
 var RockPaperGame = /** @class */ (function () {
     function RockPaperGame() {
         this.isGameInProgress = false;
+        this.gameJudge = new RockPaperJudge();
     }
     RockPaperGame.prototype.startGame = function () {
         this.isGameInProgress = true;
-        console.log("hasGameStarted: " + this.isGameInProgress);
-        // start Game
-    };
-    RockPaperGame.prototype.restartGame = function () {
-        // restart Game
+        this.gameJudge.printRules();
+        var anotherRound = "y";
+        while (anotherRound === "y" || anotherRound === "yes") {
+            var userInput = void 0;
+            do {
+                userInput = takeInput("Enter your choice (rock/paper/scissors/lizard/spock): ");
+            } while (!this.gameJudge.isLegalChoice(userInput));
+            var choiceSize = Object.keys(choice).length / 2;
+            var computerInput = choice[generateRandom(choiceSize - 1)];
+            printString("\nYour choice is: " + userInput + " \nComputer choice is: " + computerInput);
+            this.gameJudge.checkResult(userInput, computerInput);
+            anotherRound = takeInput("Play Another Round? (Y/N): ");
+        }
+        this.quitGame();
     };
     RockPaperGame.prototype.quitGame = function () {
         this.isGameInProgress = false;
-        // quits game and exit
     };
     return RockPaperGame;
 }());
--- main.ts
+++ main.ts
@@ -1,78 +1,211 @@
 "use strict";
-var require: any;
 const read = require("prompt-sync")();
 
+const printString = (str: string) => {
+  console.log(str);
+};
+
+const debug = (str: string) => {
+  // console.log(str);
+};
+
+const takeInput = (str: string) => {
+  let input: string = read(`${str}`);
+  input.trim().toLowerCase();
+  return input;
+};
+
+const generateRandom = (end: number, start: number = 0): number => {
+  return Math.floor(Math.random() * end) + start;
+};
+
+type x = string;
+
+interface SolutionType {
+  key: x;
+  value: x;
+}
+
+enum choice {
+  rock,
+  paper,
+  scissors,
+  lizard,
+  spock,
+}
+
 interface Rules {
-  winConditions(inputOne: string, inputTwo: string): string;
   printInstructions(): void;
 }
 
 interface Judge {
   winner: string;
-  checkResult(inputOne: string, inputTwo: string): string;
+  checkResult(input1: string, input2: string): void;
+  printRules(): void;
+  isLegalChoice(userChoice: string): boolean;
 }
 
 interface Game {
   isGameInProgress: Boolean;
   startGame(): void;
-  restartGame(): void;
   quitGame(): void;
 }
 
+interface Solution {
+  solutionSet: Map<x, x>;
+  generateSolutionSet(): void;
+  getValueForKey(key: x): x;
+}
+
 class Player {
-  constructor(private move: string) {}
+  private score: number;
+  constructor(private readonly move: string) {
+    this.score = 0;
+  }
   getChoice(): string {
     return this.move;
   }
-}
-
-class Input {
-  private userInput: string;
-  constructor() {}
-
-  getUserInput(): string {
-    return this.userInput;
+  getScore(): number {
+    return this.score;
   }
-
-  takeUserInput(): void {
-    // takes input from user;
+  incrementScore() {
+    this.score += 1;
   }
 }
 
-class RockPaperRules implements Rules {
-  constructor() {}
-  winConditions(inputOne: string, inputTwo: string): string {
-    return "abc";
+class RockPaperRules implements Rules, Solution {
+  solutionSet: Map<x, x>;
+  private instructionsArray: Array<string> = [
+    "Scissors cuts Paper",
+    "Paper covers Rock",
+    "Rock crushes Lizard",
+    "Lizard poisons Spock",
+    "Spock smashes Scissors",
+    "Scissors decapitates Lizard",
+    "Lizard eats Paper",
+    "Paper disproves Spock",
+    "Spock vaporizes Rock",
+    "Rock crushes Scissors",
+  ];
+  constructor() {
+    this.solutionSet = new Map();
+    this.generateSolutionSet();
+    this.printMap();
   }
+
+  printMap(): void {
+    debug("this is my map..");
+    this.solutionSet.forEach((key, value) => {
+      debug(`key: ${key} and value: ${value}`);
+    });
+  }
+
+  generateSolutionSet(): void {
+    for (let i in this.instructionsArray) {
+      let partial: Array<string> = this.instructionsArray[i]
+        .toLowerCase()
+        .split(" ");
+      let key: x = partial[0] + "," + partial[2];
+      let value: x = partial[0] + "," + partial[1];
+      this.solutionSet.set(key, value);
+      // debug("Setting value1: " + key + " " + value);
+      key = partial[2] + "," + partial[0];
+      this.solutionSet.set(key, value);
+      // debug("Setting value2: " + key + " " + value);
+    }
+  }
+
+  getValueForKey(key: x): x {
+    debug("key to be processed: " + key);
+    debug("check if exist: " + this.solutionSet.has(key));
+    if (this.solutionSet.has(key)) {
+      let value: x = this.solutionSet.get(key);
+      debug("returning value: " + value);
+      return value;
+    }
+    return "draw";
+  }
+
   printInstructions(): void {
-    //print instuctions;
+    printString(
+      "\nThis is a 2 player game and you are playing against computer...All the best!"
+    );
+    for (let i in this.instructionsArray) {
+      let partialInstruction: string = this.instructionsArray[i];
+      printString(`${i}. ${partialInstruction}`);
+    }
+    printString("\n** Enter your choice in lower case\n");
   }
 }
 
 class RockPaperJudge implements Judge {
+  private gameRules: RockPaperRules;
   winner: string;
-  constructor() {}
-  checkResult(inputOne: string, inputTwo: string): string {
-    return "abc";
+  constructor() {
+    this.gameRules = new RockPaperRules();
+  }
+  checkResult(input1: string, input2: string): void {
+    let answer: x = this.gameRules.getValueForKey(input1 + "," + input2);
+    let ans: Array<string> = answer.split(",");
+    debug("ans received: " + ans);
+    let result: string = "";
+    if (ans[0] === input1) {
+      result += `\n${ans[0]} ${ans[1]} ${input2} \nCongratulations You Won!`;
+    } else if (ans[0] === input2) {
+      result += `\n${ans[0]} ${ans[1]} ${input1} \nComputer Won...Better luck next time!`;
+    } else {
+      result += "\nNobody Wins...Draw!";
+    }
+    printString(`${result}\n`);
+  }
+
+  isLegalChoice(userChoice: string): boolean {
+    if (userChoice in choice) {
+      return true;
+    }
+    printString("Please Enter Valid Input..");
+    return false;
+  }
+  printRules(): void {
+    this.gameRules.printInstructions();
   }
 }
 
 class RockPaperGame implements Game {
-  isGameInProgress: Boolean = false;
-  constructor() {}
-  startGame(): void {
-    this.isGameInProgress = true;
-    console.log(`hasGameStarted: ${this.isGameInProgress}`);
-    // start Game
+  isGameInProgress: boolean = false;
+  private gameJudge: RockPaperJudge;
+
+  constructor() {
+    this.gameJudge = new RockPaperJudge();
   }
 
-  restartGame(): void {
-    // restart Game
+  startGame(): void {
+    this.isGameInProgress = true;
+    this.gameJudge.printRules();
+
+    let anotherRound = "y";
+    while (anotherRound === "y" || anotherRound === "yes") {
+      let userInput: string;
+
+      do {
+        userInput = takeInput(
+          "Enter your choice (rock/paper/scissors/lizard/spock): "
+        );
+      } while (!this.gameJudge.isLegalChoice(userInput));
+
+      let choiceSize = Object.keys(choice).length / 2;
+      let computerInput = choice[generateRandom(choiceSize - 1)];
+      printString(
+        `\nYour choice is: ${userInput} \nComputer choice is: ${computerInput}`
+      );
+      this.gameJudge.checkResult(userInput, computerInput);
+      anotherRound = takeInput("Play Another Round? (Y/N): ");
+    }
+    this.quitGame();
   }
 
   quitGame(): void {
     this.isGameInProgress = false;
-    // quits game and exit
   }
 }
 
